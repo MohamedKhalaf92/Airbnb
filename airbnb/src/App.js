3beb86b443928c02fa601d9319379d5327e1e66c
Mohamed Khalaf <mohamed.khalaf@infinityworks.com>
2023-03-16 15:35:20 +0000

    props

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -1,20 +1,25 @@
-import './App.css';
-import {Navbar} from "./Components/Navbar"
-import {Hero} from "./Components/Hero"
-import {Card} from "./Components/Card"
-import data from "./data"
+import "./App.css";
+import { Navbar } from "./Components/Navbar";
+import { Hero } from "./Components/Hero";
+import { Card } from "./Components/Card";
+import data from "./data";
 
 function App() {
- 
   return (
     <div className="App">
-     <Navbar/>
-     <Hero/>
-     {data.map((CardData => 
-       <Card data={CardData}/>
-       
-     ))}
-  
+      <Navbar />
+      <Hero />
+      {data.map((CardData) => (
+        <Card
+          img={CardData.coverImg}
+          title={CardData.title}
+          description={CardData.description}
+          rating={CardData.stats.rating}
+          reviewCount={CardData.stats.reviewCount}
+          location={CardData.location}
+          price={CardData.price}
+        />
+      ))}
     </div>
   );
 }
--- airbnb/src/Components/Card.js
+++ airbnb/src/Components/Card.js
@@ -6,17 +6,17 @@ export const Card = (props) => {
     <div className="card">
       <img
         className="card--image"
-        src={props.data.coverImg}
+        src={props.coverImg}
         alt=""
       />
       <div className="card--stats">
         <img className="card--star" src="../public/Images/Star 1.png" alt="" />
-        <span>{props.data.stats.rating}</span>
-        <span>({props.data.stats.reviewCount}) • </span>
-        <span>{props.data.location}</span>
+        <span>{props.rating}</span>
+        <span>({props.reviewCount}) • </span>
+        <span>{props.location}</span>
       </div>
-      <p className="card--title">{props.data.title}</p>
-      <p className="card--description">{props.data.description}</p>
+      <p className="card--title">{props.title}</p>
+      <p className="card--description">{props.description}</p>
     </div>
   );
 };
